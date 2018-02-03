c75f68d7374045051de76edb04c6cca84c797ad8
Demo User <debian@beaglebone.localdomain>
2018-02-03 05:23:17 +0000

    Made mistake with previous commit. All files updated, filament motors connected.

--- motor_test.py
+++ motor_test.py
@@ -1,22 +1,22 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
-
-delay = .000001
-GPIO.setup("P8_26", GPIO.OUT)
-GPIO.setup("P8_27", GPIO.OUT)
-GPIO.setup("P9_14", GPIO.OUT)
-GPIO.output("P8_26", GPIO.LOW)
-GPIO.output("P8_27", GPIO.LOW)
-GPIO.output("P9_14", GPIO.LOW)
-for x in range(1,6400):
-    GPIO.output("P9_14", GPIO.HIGH)
+def motor_test(en,dir,step):
+ delay = .000001
+ GPIO.setup(en, GPIO.OUT)
+ GPIO.setup(dir, GPIO.OUT)
+ GPIO.setup(step, GPIO.OUT)
+ GPIO.output(en, GPIO.LOW)
+ GPIO.output(dir, GPIO.LOW)
+ GPIO.output(step, GPIO.LOW)
+ for x in range(1,6400):
+    GPIO.output(step, GPIO.HIGH)
     time.sleep(delay)
-    GPIO.output("P9_14", GPIO.LOW)
+    GPIO.output(step, GPIO.LOW)
     time.sleep(delay)
-GPIO.output("P9_14", GPIO.HIGH)
-for x in range(1,6400):
-    GPIO.output("P9_14", GPIO.HIGH)
+ GPIO.output(dir, GPIO.HIGH)
+ for x in range(1,6400):
+    GPIO.output(step, GPIO.HIGH)
     time.sleep(delay)
-    GPIO.output("P9_14", GPIO.LOW)
+    GPIO.output(step, GPIO.LOW)
     time.sleep(delay)
-GPIO.cleanup()
+ GPIO.cleanup()
