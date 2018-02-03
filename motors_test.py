389b58378b1c457f152ef800a7e77d1d131ea459
Demo User <debian@beaglebone.localdomain>
2018-02-03 05:22:04 +0000

    Filament motors are connected

---
+++ motors_test.py
@@ -0,0 +1,15 @@
+import time
+from motor_test import motor_test
+
+motor_test("P9_11","P8_3","P8_4")
+time.sleep(1)
+motor_test("P9_12","P8_5","P8_6")
+time.sleep(1)
+motor_test("P9_13","P8_7","P8_8")
+time.sleep(1)
+motor_test("P9_14","P8_9","P8_10")
+time.sleep(1)
+motor_test("P9_15","P8_11","P8_12")
+time.sleep(1)
+motor_test("P9_16","P8_13","P8_14")
+time.sleep(1)
